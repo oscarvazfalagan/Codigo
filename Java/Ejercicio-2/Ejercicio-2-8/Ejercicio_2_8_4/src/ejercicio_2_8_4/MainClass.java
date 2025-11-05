6e9b9b5d4b2a4a036e4dc05167dcc05d348708b0
oscarvazfalagan <oscarvazfalagan@gmail.com>
2025-11-05 16:11:50 +0100

    Ejercicio-2-9

---
+++ Java/Ejercicio-2/Ejercicio-2-8/Ejercicio_2_8_4/src/ejercicio_2_8_4/MainClass.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio_2_8_4;
+
+/**
+ *
+ * @author daw1al13
+ */
+public class WorkingDay {
+    private char quenda;
+    private char day;
+    private int hours;
+}
