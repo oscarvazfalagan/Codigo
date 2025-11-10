c96d6e183abef71842831fca834f3b2913a392d4
oscarvazfalagan <oscarvazfalagan@gmail.com>
2025-11-10 10:34:19 +0100

    Ejercicio_2_3_1_completo

--- Java/Ejercicio-3/Ejercicio_3_1/Ejercicio_3_1_1/src/ejercicio_3_1_1/MainClass.java
+++ Java/Ejercicio-3/Ejercicio_3_1/Ejercicio_3_1_1/src/ejercicio_3_1_1/MainClass.java
@@ -5,7 +5,7 @@
 package ejercicio_3_1_1;
 
 /**
- *
+ * Clase MainClass que contiene el metodo main desde donde usamos la clase Marks
  * @author daw1al13
  */
 public class MainClass {
@@ -14,8 +14,9 @@ public class MainClass {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Marks mark = new Marks();
-        mark.getAverage();
+        Marks mark = new Marks(); //Creamos el objeto de la clase Marks
+        double average = mark.getAverage();//Llamamos al metodo getAverage para obtener la media de las notas
+        System.out.println("La media de tus numeros es : "+average );//Mostramos la media por pantalla
     }
     
 }
--- Java/Ejercicio-3/Ejercicio_3_1/Ejercicio_3_1_1/src/ejercicio_3_1_1/Marks.java
+++ Java/Ejercicio-3/Ejercicio_3_1/Ejercicio_3_1_1/src/ejercicio_3_1_1/Marks.java
@@ -5,32 +5,44 @@
 package ejercicio_3_1_1;
 import java.util.Scanner;
 /**
- *
+ *Clase Marks que contiene un array de 10 notas y metodos para obtener las notas y la media
  * @author daw1al13
  */
 public class Marks {
+    //Añadimos el Scanner para leer por teclado
     Scanner scan = new Scanner(System.in);
+    //Array de 10 notas
     private int marks[] = new int[10];
-    
-    
+
+    /**
+     * Metodo para obtener las notas con un bucle for
+     */
     private void getMarks(){
         System.out.println("Introduce 10 notas : ");
+        System.out.println("---------------------");
         for (int i=0; i<marks.length ; i++){
+            System.out.println("Introduce la nota " + (i+1) + " : ");
             marks[i] = scan.nextInt();
             
         }
     }
-
+    /**
+     * Constructor de la clase Marks que llama al metodo getMarks
+     * @return
+     */
     public Marks() {
         getMarks();
     }
-    public void getAverage(){
+    /**
+     * Metodo para calcular la media de las notas
+     * @return la media de las notas
+     */
+    public double getAverage(){
         Double average = 0.0;
         for (int i=0; i<marks.length ; i++){
             average += marks[i];
         }
-        System.out.println("La media de las notas es = "+ average/10);
-        
+        return average/10;
     }
     
 }
