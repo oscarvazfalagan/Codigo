4f7026ac561eb0e07548d33b81307796d835cab1
oscarvazfalagan <oscarvazfalagan@gmail.com>
2025-11-11 09:56:02 +0100

    Ejercicio 3_1_3

--- Java/Ejercicio-3/Ejercicio_3_1/Ejercicio_3_1_3/src/ejercicio_3_1_3/MainClass.java
+++ Java/Ejercicio-3/Ejercicio_3_1/Ejercicio_3_1_3/src/ejercicio_3_1_3/MainClass.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package ejercicio_3_1_3;
-
+import java.util.Scanner;
 /**
  *
  * @author daw1al13
@@ -14,7 +14,11 @@ public class MainClass {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Dame la cantidad de numeros que quieres usar : ");
+        int n = scan.nextInt();
+        Statistics numbers = new Statistics(n);
+         
     }
     
 }
--- Java/Ejercicio-3/Ejercicio_3_1/Ejercicio_3_1_3/src/ejercicio_3_1_3/Statistics.java
+++ Java/Ejercicio-3/Ejercicio_3_1/Ejercicio_3_1_3/src/ejercicio_3_1_3/Statistics.java
@@ -9,5 +9,27 @@ package ejercicio_3_1_3;
  * @author daw1al13
  */
 public class Statistics {
+    private int numbers[];
     
+    private void getNumbers() {
+        System.out.println("Introduce 10 notas : ");
+        System.out.println("---------------------");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Introduce la nota " + (i + 1) + " : ");
+            
+
+        }
+    }
+
+    public Statistics(int n) {
+        if (n%2 != 0) {
+            int numbers[] = new int[n];
+            getNumbers();
+        }
+        else {
+            System.out.println("No se puede ejecutar");
+        }
+    }
+
+
 }
