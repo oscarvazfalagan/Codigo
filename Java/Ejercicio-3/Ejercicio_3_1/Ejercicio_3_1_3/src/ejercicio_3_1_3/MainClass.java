ed393a9ca7964dddc702650ec0ae13429e5a6051
oscarvazfalagan <oscarvazfalagan@gmail.com>
2025-11-12 13:53:24 +0100

    comit

--- Java/Ejercicio-3/Ejercicio_3_1/Ejercicio_3_1_3/src/ejercicio_3_1_3/MainClass.java
+++ Java/Ejercicio-3/Ejercicio_3_1/Ejercicio_3_1_3/src/ejercicio_3_1_3/MainClass.java
@@ -5,7 +5,7 @@
 package ejercicio_3_1_3;
 import java.util.Scanner;
 /**
- *
+ * Class MainClass que crea un objeto de la clase Statistics y muestra la mediana de los numeros introducidos
  * @author daw1al13
  */
 public class MainClass {
@@ -14,11 +14,12 @@ public class MainClass {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in); //Creamos el objeto scan de la clase Scanner
+        //Pedimos al usuario la cantidad de numeros que quiere usar
         System.out.println("Dame la cantidad de numeros que quieres usar : ");
         int n = scan.nextInt();
-        Statistics numbers = new Statistics(n);
-        System.out.println("La mediana de tus numeros es : "+numbers.median());
+        Statistics numbers = new Statistics(n);//Creamos el objeto numbers de la clase Statistics con el numero de elementos n
+        System.out.println("La mediana de tus numeros es : "+numbers.median()); //Mostramos la mediana de los numeros introducidos
          
     }
     
--- Java/Ejercicio-3/Ejercicio_3_1/Ejercicio_3_1_3/src/ejercicio_3_1_3/Statistics.java
+++ Java/Ejercicio-3/Ejercicio_3_1/Ejercicio_3_1_3/src/ejercicio_3_1_3/Statistics.java
@@ -5,24 +5,34 @@
 package ejercicio_3_1_3;
 import java.util.Scanner;
 /**
- *
+ * Class Statistics que almacena un array de numeros enteros y tiene metodos para obtener la mediana
  * @author daw1al13
  */
 public class Statistics {
     Scanner scan = new Scanner(System.in);
-    private int numbers[];
+    private int numbers[]; // Array de enteros para las notas
     
+    /**
+     * Metodo privado getNumbers que pide al usuario que introduzca 10 numeros enteros
+     *  y los almacena en el array numbers
+     */
     private void getNumbers() {
         System.out.println("Introduce 10 notas : ");
         System.out.println("---------------------");
         for (int i = 0; i < numbers.length; i++) {
-            System.out.println("Introduce la nota " + (i + 1) + " : ");
+            System.out.println("Introduce el numero " + (i + 1) + " : ");
             numbers[i] = scan.nextInt();
 
         }
     }
+    /**
+     * Constructor de la clase Statistics que inicializa el array numbers
+     *  y llama al metodo getNumbers para llenarlo
+     * @param n numero de elementos del array numbers
+     */
 
     public Statistics(int n) {
+        //Si el numero es impar no deja continuar
         if (n%2 != 0) {
             this.numbers = new int[n];
             getNumbers();
@@ -31,26 +41,33 @@ public class Statistics {
             System.out.println("No se puede ejecutar");
         }
     }
-    
+    /**
+     * Metodo publico median que devuelve la mediana de los numeros almacenados en el array numbers
+     * @return mediana de los numeros del array numbers
+     */
     public int median(){
-        int mediana = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            int contadorMin = 0;
-            int contadorMax = 0;
+        int mediana = 0; //Declaro la variable mediana
+        boolean exit = false; 
+        //Bucle doble para contar cuantos numeros son mayores y menores que el numero actual
+        for (int i = 0; i < numbers.length && !exit; i++) {
+            int contadorMin = 0; //Contador de numeros menores
+            int contadorMax = 0; //Contador de numeros mayores
             for (int j = 0; j < numbers.length; j++) {
                 if (numbers[i]>numbers[j]){
-                    contadorMin = contadorMin + 1;
+                    contadorMin = contadorMin + 1; // Incrementa cuando un numero es mayor
                 }
                 if (numbers[i]<numbers[j]){
-                    contadorMax = contadorMax + 1;
+                    contadorMax = contadorMax + 1; // Incrementa cuando un numero es menot
                 }
                 
             }
+            //Si los contadores son iguales significa que el numero es la mediana
             if (contadorMax == contadorMin){
                     mediana = numbers[i];
+                    exit = true; // Salimos del bucle
                 }
         }
-        return mediana;
+        return mediana; 
     }
 
 }
