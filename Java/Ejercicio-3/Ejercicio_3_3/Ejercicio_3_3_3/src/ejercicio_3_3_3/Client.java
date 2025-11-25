5ab8c20e39121e059098625ef7628995e0f3f139
oscarvazfalagan <oscarvazfalagan@gmail.com>
2025-11-25 12:07:25 +0100

    save

--- Java/Ejercicio-3/Ejercicio_3_3/Ejercicio_3_3_3/src/ejercicio_3_3_3/Client.java
+++ Java/Ejercicio-3/Ejercicio_3_3/Ejercicio_3_3_3/src/ejercicio_3_3_3/Client.java
@@ -9,5 +9,39 @@ package ejercicio_3_3_3;
  * @author daw1al13
  */
 public class Client {
+    private String nif;
+    private String name;
+    private String surname;
+
+    public String getNif() {
+        return nif;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Client(String nif, String name, String surname) {
+        this.nif = nif;
+        this.name = name;
+        this.surname = surname;
+    }
+    
     
 }
