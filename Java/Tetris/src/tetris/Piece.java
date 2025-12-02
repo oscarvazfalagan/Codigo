cac0d06220e899dc937a03c0bdf3f7e8bb2797d1
oscarvazfalagan <oscarvazfalagan@gmail.com>
2025-12-02 09:39:33 +0100

    changes

--- Java/Tetris/src/tetris/Piece.java
+++ Java/Tetris/src/tetris/Piece.java
@@ -16,7 +16,7 @@
  */
 package tetris;
 
-
+import java.awt.Color;
 
 /**
  * Clase que implementa a peza cadrada do xogo do Tetris
@@ -83,11 +83,11 @@ public class Piece {
             c.setX(c.getX()-20);
             d.setX(d.getX()-20);
             canMove=true;
-            
         }
         return canMove;
     }
-   
+
+
     /**
      * Move a ficha a abaixo se é posible
      *
